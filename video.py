1946368985f0d7ce937ae0a2d9348a5f58ab5200
saltchicken <john.eicher89@gmail.com>
2022-06-04 19:48:06 -0700

    Adding monospace font and draw via PIL

--- video.py
+++ video.py
@@ -2,14 +2,14 @@ import cv2
 import os
 import numpy as np
 import pyvirtualcam
+from PIL import Image, ImageFont, ImageDraw
 
 ########################################################################
 # SETTINGS #
-WIDTH = 1280
-HEIGHT = 720
-FPS = 20
+WIDTH = 640
+HEIGHT = 360
+FPS = 60
 ########################################################################
-
 
 def convert_row_to_ascii(row):
     # 17-long
@@ -17,35 +17,53 @@ def convert_row_to_ascii(row):
              'x', 'o', 'k', 'X', 'd', 'O', '0', 'K', 'N')
     return tuple(ORDER[int(x / (255 / 16))] for x in row)[::-1]
 
-
 def convert_to_ascii(input_grays):
     return tuple(convert_row_to_ascii(row) for row in input_grays)
 
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
 
 with pyvirtualcam.Camera(width=WIDTH, height=HEIGHT, fps=FPS, print_fps=True) as cam:
-	print(f'Using virtual camera: {cam.device}')
-	while(cv2.waitKey(1) & 0xFF != ord('q')):
-		screen_height, screen_width = height, width
+    print(f'Using virtual camera: {cam.device}')
 
-		# Get image data
-		ret, frame = cap.read()
+    monospace = ImageFont.truetype("./Fonts/ANDALEMO.ttf",32)
+    text = "Test"
+    textSize = monospace.getsize(text)
+    
+    frame_blank = np.zeros((cam.height, cam.width, 3), np.uint8)  # RGB
+    while(cv2.waitKey(1) & 0xFF != ord('q')):
 
-		# Convert data to grayscale
-		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Get image data
+        ret, frame = cap.read()
 
-		# Reduce grayscale array to proper resolution
-		reduced = cv2.resize(gray, (int(screen_width), int(screen_height)))
+        ## Convert data to grayscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-		# Plug in reduced resolution numpy array for ascii converter func
-		converted = convert_to_ascii(reduced)
-			
-		frame = np.zeros((cam.height, cam.width, 3), np.uint8)  # RGB
-		frame[:] = cam.frames_sent % 255  # grayscale animation
-		cam.send(frame)
-		cam.sleep_until_next_frame()
+        ## Reduce grayscale array to proper resolution
+        reduced = cv2.resize(gray, (int(cam.width), int(cam.height)))
+        # cv2.imshow('frame', reduced)
+        
+		# Make PIL image
+        im_p = Image.fromarray(frame_blank)
 
+		# Get a drawing context
+        draw = ImageDraw.Draw(im_p)
+		
+        ## Plug in reduced resolution numpy array for ascii converter func
+        converted = convert_to_ascii(reduced)    
+		
+		## TODO use `converted` as text input
+        draw.text((40, 80),text,(255,255,255),font=monospace)
 
-# When everything done, release the capture
+		# Convert back to OpenCV image and save
+        result_o = np.array(im_p)
+
+        cv2.imshow('frame', result_o)
+            
+        # frame_blank[:] = cam.frames_sent % 255  # grayscale animation
+        # cam.send(frame_blank)
+        # cam.sleep_until_next_frame()
+
 cap.release()
 cv2.destroyAllWindows()
