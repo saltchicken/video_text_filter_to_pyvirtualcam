f5edb5c0c30b2d3498e6cf2edd6ce67da096c13e
saltchicken <john.eicher89@gmail.com>
2022-06-05 17:26:20 -0700

    Conversion is working and sends to virtual camera. Needs to be optimized.

--- video.py
+++ video.py
@@ -6,13 +6,12 @@ from PIL import Image, ImageFont, ImageDraw
 
 ########################################################################
 # SETTINGS #
-WIDTH = 640
-HEIGHT = 360
+WIDTH = 1920//2
+HEIGHT = 1080//2
 FPS = 60
 ########################################################################
 
 def convert_row_to_ascii(row):
-    # 17-long
     ORDER = (' ', '.', "'", ',', ':', ';', 'c', 'l',
              'x', 'o', 'k', 'X', 'd', 'O', '0', 'K', 'N')
     return tuple(ORDER[int(x / (255 / 16))] for x in row)[::-1]
@@ -20,50 +19,50 @@ def convert_row_to_ascii(row):
 def convert_to_ascii(input_grays):
     return tuple(convert_row_to_ascii(row) for row in input_grays)
 
+## TODO Optimize camera resolution.
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
 
 with pyvirtualcam.Camera(width=WIDTH, height=HEIGHT, fps=FPS, print_fps=True) as cam:
     print(f'Using virtual camera: {cam.device}')
 
-    monospace = ImageFont.truetype("./Fonts/ANDALEMO.ttf",32)
+    ## Determine proper font size
+    monospace = ImageFont.truetype("./Fonts/ANDALEMO.ttf",16)
+    
+    ## TODO Use textSize to optimize dimensions for ascii output
     text = "Test"
     textSize = monospace.getsize(text)
     
-    frame_blank = np.zeros((cam.height, cam.width, 3), np.uint8)  # RGB
+    frame_background = np.zeros((cam.height, cam.width, 3), np.uint8)  # RGB
     while(cv2.waitKey(1) & 0xFF != ord('q')):
 
         # Get image data
         ret, frame = cap.read()
 
-        ## Convert data to grayscale
+        # Convert data to grayscale
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        ## Reduce grayscale array to proper resolution
-        reduced = cv2.resize(gray, (int(cam.width), int(cam.height)))
-        # cv2.imshow('frame', reduced)
+        ## TODO Optimize dimensions
+        # Reduce grayscale array to proper resolution
+        reduced = cv2.resize(gray, (80, 45))
         
-		# Make PIL image
-        im_p = Image.fromarray(frame_blank)
+        # Make PIL image
+        im_p = Image.fromarray(frame_background)
 
-		# Get a drawing context
+        # Get a drawing context
         draw = ImageDraw.Draw(im_p)
-		
-        ## Plug in reduced resolution numpy array for ascii converter func
+        
+        ## TODO Optimize indexing.
+        # Plug in reduced resolution numpy array for ascii converter func
         converted = convert_to_ascii(reduced)    
-		
-		## TODO use `converted` as text input
-        draw.text((40, 80),text,(255,255,255),font=monospace)
+        for index, row in enumerate(converted):
+            draw.text((0, index * 12),''.join(row),(255,255,255),font=monospace)
 
-		# Convert back to OpenCV image and save
+        # Convert back to OpenCV image and save
         result_o = np.array(im_p)
 
-        cv2.imshow('frame', result_o)
-            
-        # frame_blank[:] = cam.frames_sent % 255  # grayscale animation
-        # cam.send(frame_blank)
-        # cam.sleep_until_next_frame()
+        cam.send(result_o)
 
 cap.release()
 cv2.destroyAllWindows()
